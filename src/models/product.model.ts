a653b8e718b089833d8730485731ac9e3bf80092
elias.caracas <elias.caracas@eggcooperation.com>
2023-04-26 11:36:31 -0300

    add product

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,32 +1,50 @@
 import mongoose from "mongoose";
+import { customAlphabet } from "nanoid";
 import { UserDocument } from "./user.model";
 
-export interface SessionDocument extends mongoose.Document {
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 10);
+
+export interface ProductInput {
   user: UserDocument["_id"];
-  valid: boolean;
-  userAgent: string;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+}
+
+export interface ProductDocument extends ProductInput, mongoose.Document {
+  user: UserDocument["_id"];
+  title: string;
+  description: string;
+  price: number;
+  image: string;
   createdAt: Date;
   updatedAt: Date;
   comparePassword(candidatePassword: string): Promise<Boolean>;
 }
 
-const sessionSchema = new mongoose.Schema(
+const productSchema = new mongoose.Schema(
   {
+    productId: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => `product_${nanoid()}`,
+    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    valid: {
-      type: Boolean,
-      default: true,
-    },
-    userAgent: { type: String },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
   },
   {
     timestamps: true,
   }
 );
 
-const SessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
+const ProductModel = mongoose.model<ProductDocument>("Product", productSchema);
 
-export default SessionModel;
+export default ProductModel;
